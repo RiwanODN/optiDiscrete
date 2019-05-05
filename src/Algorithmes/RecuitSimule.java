735a96053e109cbaab0a3de01af1033604d04929
Jkeez <cebrail.yalcin.sch@gmail.com>
2019-05-05 21:59:03 +0200

    recuitSimule
    recuit simule + generation solution aleatoire + generation voisin

--- src/Algorithmes/RecuitSimule.java
+++ src/Algorithmes/RecuitSimule.java
@@ -1,5 +1,8 @@
 package Algorithmes;
 
+
+import java.util.Random;
+
 public class RecuitSimule {
 
     public int[][] distance;
@@ -38,4 +41,96 @@ public class RecuitSimule {
     }
 
 
+    public int[] effectuerRecuitSimule(int[] solutionInitiale, double temperatureInitiale,
+                                       int nombreIteration, double tauxRefroidissement){
+
+        System.out.println("------ RECUIT SIMULE ------");
+        //Initialisation
+        int[] solutionMinimale = solutionInitiale;
+        int fitnessMinimale = this.calculerFitness(solutionInitiale);
+
+        int[] solutionActuelle;
+        int fitnessActuelle;
+
+        int delta;
+
+        int[] solutionTmp;
+        int fitnessTmp;
+
+        double probabiliteAcceptation;
+        double temperatureActuelle = temperatureInitiale;
+
+
+        for(int i = 0 ; i <= nombreIteration ; i++){
+            //On décide d'arrêter si la température est beaucoup trop faible
+            if(temperatureInitiale < 0.1){
+                System.out.println("interruption : temperature < 0.1");
+                return solutionMinimale;
+            }
+            for (int j = 0; j<=50 ; j++){
+                //Generation d'un voisin aleatoire
+                solutionActuelle = this.genererVoisinAleatoire(solutionMinimale.clone());
+                //Fitness du voisin generer
+                fitnessActuelle = this.calculerFitness(solutionActuelle);
+
+                //Difference de la fitness du voisin et la fitness minimale
+                delta = fitnessActuelle - fitnessMinimale;
+
+               // System.out.println("Fmin : "+fitnessMinimale+" Fi : "+fitnessActuelle+ " delta : "+delta);
+                if(delta <= 0){
+                    //System.out.println("delta <= 0");
+                    //On sauvegarde la meilleure solution afin de la restaurer si necessaire
+                    solutionTmp = solutionMinimale;
+                    fitnessTmp = fitnessMinimale;
+
+                    solutionMinimale = solutionActuelle;
+                    fitnessMinimale = fitnessActuelle;
+
+                } else {
+
+                    probabiliteAcceptation = Math.random();
+                    //System.out.println(Math.exp(( (delta * -1) / temperatureActuelle)) +
+                         //   " > "+ probabiliteAcceptation);
+
+                    if(Math.exp(( (delta * -1) / temperatureActuelle)) > probabiliteAcceptation){
+                       // System.out.println("Formule");
+                        solutionMinimale = solutionActuelle;
+                        fitnessMinimale = fitnessActuelle;
+                    }
+                }
+            }
+
+
+
+            //Refroidissement de la temperature
+            temperatureActuelle = temperatureActuelle * tauxRefroidissement;
+
+        }
+
+
+        return solutionMinimale;
+
+    }
+
+    public int[] genererVoisinAleatoire(int[] solutionActuelle){
+        Random intAleatoire = new Random();
+
+        int nombreElement = solutionActuelle.length;
+        int premierElement = 0;
+        int deuxiemeElement = 0;
+
+        //index aleatoire entre 0 et nombreElement
+        premierElement = intAleatoire.nextInt(nombreElement);
+
+        while(deuxiemeElement == premierElement){
+            deuxiemeElement = intAleatoire.nextInt(nombreElement);
+        }
+
+        //Permuttation des valeurs
+        int tmp = solutionActuelle[premierElement];
+        solutionActuelle[premierElement] = solutionActuelle[deuxiemeElement];
+        solutionActuelle[deuxiemeElement] = tmp;
+
+        return solutionActuelle;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 import Algorithmes.RecuitSimule;
 import Outils.LecteurFichier;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -8,9 +12,12 @@ public class Main {
         System.out.println("QAP");
         System.out.println("http://anjos.mgi.polymtl.ca/qaplib/inst.html#Ta");
 
-        int[] solutionEssai = {8,1,6,2,11,10,3,5,9,7,12,4};
+        Integer[] solutionEssai = new Integer[]{1,2,3,4,5,6,7,8,9,10,11,12};
 
-        LecteurFichier lecteur = new LecteurFichier("tai12a.txt");
+        List<Integer> solutionAleatoireList = Arrays.asList(solutionEssai);
+        Collections.shuffle(solutionAleatoireList);
+
+        LecteurFichier lecteur = new LecteurFichier("tai12a.txt");//Smin = {8,1,6,2,11,10,3,5,9,7,12,4} Fmin = 224416
 
         lecteur.lireFichier();
 
@@ -20,7 +27,40 @@ public class Main {
         RecuitSimule recuitSimule = new RecuitSimule(lecteur.getDistances(), lecteur.getPoids());
 
 
+        //Parametres recuit simule :
+        //int[] solutionInitiale, double temperatureInitiale,
+        //int nombreIteration, double tauxRefroidissement
 
-        System.out.println("Fitness : "+ recuitSimule.calculerFitness(solutionEssai));
+        //Affichage solution initiale
+        System.out.println("Solution initiale : ");
+        Main.afficherSolution(solutionAleatoireList.stream().mapToInt(Integer::intValue).toArray());
+        System.out.println("Fitness : " + recuitSimule.calculerFitness(solutionAleatoireList.stream().mapToInt(Integer::intValue).toArray()));
+
+        //Solution du recuit Simule
+        int[] solutionRecuitSimule = recuitSimule.effectuerRecuitSimule(
+                solutionAleatoireList.stream().mapToInt(Integer::intValue).toArray()
+                , 15000,
+                1000, 0.99);
+
+        //Afficher solution du recuit simule
+        System.out.println("Solution du recuit simule : ");
+        Main.afficherSolution(solutionRecuitSimule);
+        System.out.println("fitness du recuit simule : " +
+                recuitSimule.calculerFitness(solutionRecuitSimule));
+
+
+
+    }
+
+    public static void afficherSolution(int[] solution){
+        System.out.print("(");
+        for(int i = 0; i<solution.length;i++){
+            System.out.print(solution[i]);
+            if(i ==solution.length-1){
+                System.out.println(")");
+            }else{
+                System.out.print(" , ");
+            }
+        }
     }
 }
