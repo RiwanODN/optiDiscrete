f48cab3a6326c277132a3e284e6254bba29bfbba
RiwanODN <riwan-69@hotmail.fr>
2019-06-01 13:25:58 +0200

    Tabou lunchable

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import Algorithmes.RecuitSimule;
+import Algorithmes.Tabou;
 import Outils.LecteurFichier;
 
 import java.util.Arrays;
@@ -7,7 +8,7 @@ import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
 
         System.out.println("QAP");
         System.out.println("http://anjos.mgi.polymtl.ca/qaplib/inst.html#Ta");
@@ -47,6 +48,62 @@ public class Main {
         Main.afficherSolution(solutionRecuitSimule);
         System.out.println("fitness du recuit simule : " +
                 recuitSimule.calculerFitness(solutionRecuitSimule));
+
+
+
+    }
+
+    public static void afficherSolution(int[] solution){
+        System.out.print("(");
+        for(int i = 0; i<solution.length;i++){
+            System.out.print(solution[i]);
+            if(i ==solution.length-1){
+                System.out.println(")");
+            }else{
+                System.out.print(" , ");
+            }
+        }
+    }*/
+
+    public static void main(String[] args) {
+
+        System.out.println("QAP");
+        System.out.println("http://anjos.mgi.polymtl.ca/qaplib/inst.html#Ta");
+
+        Integer[] solutionEssai = new Integer[]{1,2,3,4,5,6,7,8,9,10,11,12};
+
+        List<Integer> solutionAleatoireList = Arrays.asList(solutionEssai);
+        Collections.shuffle(solutionAleatoireList);
+
+        LecteurFichier lecteur = new LecteurFichier("tai12a.txt");//Smin = {8,1,6,2,11,10,3,5,9,7,12,4} Fmin = 224416
+
+        lecteur.lireFichier();
+
+        lecteur.afficherPoids();
+        lecteur.afficherDistances();
+
+        Tabou Tabou = new Tabou(lecteur.getDistances(), lecteur.getPoids());
+
+
+        //Parametres recuit simule :
+        //int[] solutionInitiale, double temperatureInitiale,
+        //int nombreIteration, double tauxRefroidissement
+
+        //Affichage solution initiale
+        System.out.println("Solution initiale : ");
+        Main.afficherSolution(solutionAleatoireList.stream().mapToInt(Integer::intValue).toArray());
+        System.out.println("Fitness : " + Tabou.calculerFitness(solutionAleatoireList.stream().mapToInt(Integer::intValue).toArray()));
+
+        //Solution du recuit Simule
+        int[] solutionRecuitSimule = Tabou.effectuerTabou(
+                solutionAleatoireList.stream().mapToInt(Integer::intValue).toArray()
+                , 1000);
+
+        //Afficher solution du recuit simule
+        System.out.println("Solution du recuit simule : ");
+        Main.afficherSolution(solutionRecuitSimule);
+        System.out.println("fitness du recuit simule : " +
+                Tabou.calculerFitness(solutionRecuitSimule));
 
 
 
