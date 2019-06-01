ab676bfaaa53ac6bd0ecd83d3e4484cc4aff413a
RiwanODN <riwan-69@hotmail.fr>
2019-06-01 12:54:35 +0200

    Update Tabou.java

--- src/Algorithmes/Tabou.java
+++ src/Algorithmes/Tabou.java
@@ -1,6 +1,10 @@
 package Algorithmes;
 
-import java.util.Random;
+import Solution.Voisin;
+import javafx.util.Pair;
+
+import java.sql.SQLOutput;
+import java.util.*;
 
 public class Tabou{
 
@@ -35,74 +39,67 @@ public class Tabou{
             }
 
         }
-
         return fitness;
     }
 
 
-    public int[] effectuerRecuitSimule(int[] solutionInitiale, double temperatureInitiale,
-                                       int nombreIteration, double tauxRefroidissement){
+    public int[] effectuerTabou(int[] solutionInitiale, int nombreIteration){
 
-        System.out.println("------ RECUIT SIMULE ------");
+
+        System.out.println("------ Tabou ------");
+
         //Initialisation
         int[] solutionMinimale = solutionInitiale;
         int fitnessMinimale = this.calculerFitness(solutionInitiale);
 
-        int[] solutionActuelle;
-        int fitnessActuelle;
+       /* int[] solutionActuelle=solutionInitiale;
+        int fitnessActuelle=this.calculerFitness(solutionActuelle);*/
 
         int delta;
 
         int[] solutionTmp;
         int fitnessTmp;
 
-        double probabiliteAcceptation;
-        double temperatureActuelle = temperatureInitiale;
-
+        ArrayList<int[]> mouvementInterdit=new ArrayList<>();
 
         for(int i = 0 ; i <= nombreIteration ; i++){
-            //On décide d'arrêter si la température est beaucoup trop faible
-            if(temperatureInitiale < 0.1){
-                System.out.println("interruption : temperature < 0.1");
-                return solutionMinimale;
-            }
-            for (int j = 0; j<=50 ; j++){
+
                 //Generation d'un voisin aleatoire
-                solutionActuelle = this.genererVoisinAleatoire(solutionMinimale.clone());
+                ArrayList<Voisin> voisins= this.genererVoisinAleatoire(solutionMinimale.clone(),mouvementInterdit);
+
                 //Fitness du voisin generer
-                fitnessActuelle = this.calculerFitness(solutionActuelle);
+                voisins = this.calculerFitnessVoisins(voisins,fitnessActuelle);
+            System.out.println("FA "+fitnessActuelle);
+            //System.out.println("FV1 "+voisins.get(0).getFitness());
+           // System.out.println("FV2 "+voisins.get(1).getFitness());
 
-                //Difference de la fitness du voisin et la fitness minimale
-                delta = fitnessActuelle - fitnessMinimale;
+                //selection fitness la plus faible
+                Voisin meilleurVoisin=this.chercherFitnessMin(voisins);
+            System.out.println("F M "+meilleurVoisin.getFitness());
+            System.out.println("D M" + meilleurVoisin.getDelta());
 
-               // System.out.println("Fmin : "+fitnessMinimale+" Fi : "+fitnessActuelle+ " delta : "+delta);
-                if(delta <= 0){
+
+                // System.out.println("Fmin : "+fitnessMinimale+" Fi : "+fitnessActuelle+ " delta : "+delta);
+                if(meilleurVoisin.getDelta() <= 0){
                     //System.out.println("delta <= 0");
-                    //On sauvegarde la meilleure solution afin de la restaurer si necessaire
-                    solutionTmp = solutionMinimale;
-                    fitnessTmp = fitnessMinimale;
 
-                    solutionMinimale = solutionActuelle;
-                    fitnessMinimale = fitnessActuelle;
+                    solutionMinimale = meilleurVoisin.getSolution() ;
+                    fitnessMinimale = meilleurVoisin.getFitness();
 
                 } else {
 
-                    probabiliteAcceptation = Math.random();
-                    //System.out.println(Math.exp(( (delta * -1) / temperatureActuelle)) +
-                         //   " > "+ probabiliteAcceptation);
+                    /*solutionMinimale = meilleurVoisin.getSolution() ;
+                    fitnessMinimale = meilleurVoisin.getFitness();*/
+                    mouvementInterdit=this.majMouvementInterdit(mouvementInterdit,meilleurVoisin);
 
-                    if(Math.exp(( (delta * -1) / temperatureActuelle)) > probabiliteAcceptation){
+
+
+                    /*if(Math.exp(( (delta * -1) / temperatureActuelle)) > probabiliteAcceptation){
                        // System.out.println("Formule");
                         solutionMinimale = solutionActuelle;
                         fitnessMinimale = fitnessActuelle;
-                    }
+                    }*/
                 }
-            }
-
-
-
-            //Refroidissement de la temperature
-            temperatureActuelle = temperatureActuelle * tauxRefroidissement;
 
         }
 
@@ -111,25 +108,131 @@ public class Tabou{
 
     }
 
-    public int[] genererVoisinAleatoire(int[] solutionActuelle){
+    public ArrayList genererVoisinAleatoire(int[] solutionActuelle, ArrayList<int[]> listMouvementInterdit){
+        int tmp;
+        int mouvementInterdit[];
+
+        ArrayList<Voisin> voisins= new ArrayList<>();
         Random intAleatoire = new Random();
 
         int nombreElement = solutionActuelle.length;
         int premierElement = 0;
-        int deuxiemeElement = 0;
+
+        System.out.println("recherche de voisins");
 
         //index aleatoire entre 0 et nombreElement
-        premierElement = intAleatoire.nextInt(nombreElement);
+        premierElement = 1+intAleatoire.nextInt(nombreElement-2);
 
-        while(deuxiemeElement == premierElement){
-            deuxiemeElement = intAleatoire.nextInt(nombreElement);
+        //Permuttation de xi et xi-1
+        int deuxiemeElement = premierElement-1;
+        int[] sVoisin= Arrays.copyOf(solutionActuelle,nombreElement);
+
+        //boolean permettant de savoir si le mouvement est non autorisé
+        boolean b=false;
+        if (listMouvementInterdit.size()!=0){
+
+
+
+        for(int i=0;i<=listMouvementInterdit.size()-1;i++) {
+            if (listMouvementInterdit.get(i)[0] == premierElement && listMouvementInterdit.get(i)[1] == deuxiemeElement){
+                b = true;
+
+            }
+        }
         }
 
-        //Permuttation des valeurs
-        int tmp = solutionActuelle[premierElement];
-        solutionActuelle[premierElement] = solutionActuelle[deuxiemeElement];
-        solutionActuelle[deuxiemeElement] = tmp;
+        if (b==false){
+            tmp = sVoisin[premierElement];
 
-        return solutionActuelle;
+
+
+            sVoisin[premierElement] = sVoisin[deuxiemeElement];
+            sVoisin[deuxiemeElement] = tmp;
+
+            voisins.add(new Voisin(sVoisin));
+
+            //ajout du mouvement interdit
+
+            mouvementInterdit=new int[]{deuxiemeElement,premierElement};
+            voisins.get(0).setMouvementInterdit(mouvementInterdit);
+        }
+
+
+        //Permuttation de xi et xi+1
+        sVoisin= Arrays.copyOf(solutionActuelle,nombreElement);
+        deuxiemeElement = premierElement+1;
+
+        b=false;
+        if (listMouvementInterdit.size()!=0) {
+            //verification que le mouvement n'est pas non autorisée
+            for (int i = 0; i <= listMouvementInterdit.size()-1; i++) {
+                if (listMouvementInterdit.get(i)[0] == premierElement && listMouvementInterdit.get(i)[1] == deuxiemeElement)
+                    b = true;
+
+            }
+        }
+
+        if (b==false){
+            //System.out.println("deuxieme ele"+ deuxiemeElement);
+
+            tmp = sVoisin[premierElement];
+
+            sVoisin[premierElement] = sVoisin[deuxiemeElement];
+            sVoisin[deuxiemeElement] = tmp;
+
+            voisins.add(new Voisin(sVoisin));
+
+            //ajout du mouvement interdit
+            //System.out.println("un deuxieme ajout de mvt");
+            mouvementInterdit = new int[]{premierElement, deuxiemeElement};
+            voisins.get(1).setMouvementInterdit(mouvementInterdit);
+        }
+        return voisins;
+    }
+
+
+    public ArrayList<Voisin> calculerFitnessVoisins(ArrayList<Voisin> voisins, int fitnessActuelle){
+        //calcul la fitness des voisins
+        HashMap<int[], Pair<Integer,Integer>> map = new HashMap<>();
+
+        for(int i=0; i<=voisins.size()-1;i++){
+            voisins.get(i).setFitness(this.calculerFitness(voisins.get(i).getSolution()));
+            voisins.get(i).setDelta(voisins.get(i).getFitness()-fitnessActuelle);
+        }
+        return voisins;
+    }
+
+    public Voisin chercherFitnessMin(ArrayList<Voisin> voisins){
+
+        int fitMin = voisins.get(0).getFitness();
+        int iMin=0;
+
+        for (int i =1;i<=voisins.size()-1;i++){
+
+            if (fitMin>voisins.get(i).getFitness()){
+                fitMin=voisins.get(i).getFitness();
+                iMin=i;
+            }
+        }
+
+       // HashMap<int[], Pair> meilleurVoisin= new HashMap<>();
+        //meilleurVoisin.put(voisins.get(iMin),map.get(voisins.get(iMin)));
+
+        return voisins.get(iMin);
+    }
+
+    public ArrayList<int[]> majMouvementInterdit(ArrayList<int[]> mouvementInterdit, Voisin meilleurVoisin){
+
+
+        mouvementInterdit.add(0,meilleurVoisin.getMouvementInterdit());
+
+        if (mouvementInterdit.size()>10){
+            mouvementInterdit.remove(mouvementInterdit.size()-1);
+        }
+
+
+        return mouvementInterdit;
     }
 }
+
+
